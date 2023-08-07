d93069f42a3bef5001c8c31ade67653f8bedae81
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-08-07 22:09:15 +0530

    added file upload using multer

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 const port = 8181;
@@ -12,6 +13,27 @@ const port = 8181;
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single('file'), function (req, res) {
+    try {
+        const file = req.file;
+        return res.status(201).json(file.filename);
+    } catch (error) {
+        console.log({error: error.message});
+    }
+})
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
